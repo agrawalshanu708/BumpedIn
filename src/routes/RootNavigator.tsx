92a767fa1be9d870aeee1ac58d55796294376a07
shanuagrawal <agrawalshanu798@gmail.com>
2023-12-06 17:09:09 +0530

    added form page

--- src/routes/RootNavigator.tsx
+++ src/routes/RootNavigator.tsx
@@ -4,7 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { ROUTES } from './Routes';
 import WelcomeScreen from './../features/welcomeScreen/WelcomeScreen';
 import LoginError from './../features/loginError/LoginError';
-// import Form from 'features/form/Form';
+import Form from './../features/form/Form';
 // import ProfileInfo from 'features/profileInfo/ProfileInfo';
 // import TabNavigator from './TabNavigator';
 // import EditForm from 'features/editForm/EditForm';
@@ -22,8 +22,8 @@ const RootNavigator = () => (
             component={WelcomeScreen}>
         </Stack.Screen>
         <Stack.Screen name={ROUTES.LOGIN_ERROR.name} component={LoginError}></Stack.Screen>
-        {/* <Stack.Screen name={ROUTES.FORM.name} component={Form}></Stack.Screen>
-        <Stack.Screen name={ROUTES.PROFILE_VIEW.name} component={ProfileInfo}></Stack.Screen>
+        <Stack.Screen name={ROUTES.FORM.name} component={Form}></Stack.Screen>
+        {/* <Stack.Screen name={ROUTES.PROFILE_VIEW.name} component={ProfileInfo}></Stack.Screen>
         <Stack.Screen name={ROUTES.TAB_NAVIGATOR_ROOT.name} component={TabNavigator}></Stack.Screen>
         <Stack.Screen name={ROUTES.EDIT_FORM.name} component={EditForm}></Stack.Screen>
         <Stack.Screen name={ROUTES.MATCH_SUCCESS.name} component={NewMatch}></Stack.Screen> */}
