6de0153fef5dc59509c8929fe7622e9ca32facab
shanuagrawal <agrawalshanu798@gmail.com>
2023-12-08 17:32:35 +0530

    add fonts

--- src/features/welcomeScreen/WelcomeScreenStatics.ts
+++ src/features/welcomeScreen/WelcomeScreenStatics.ts
@@ -1,7 +1,7 @@
 export const WELCOME_SCREEN_STATICS = {
   HEADER: {
     heading: 'Welcome to Bumpedin!',
-    subHeading: 'Connect with ISB alums around you ju',
+    subHeading: 'Connect with ISB alums around you',
   },
   SIGNIN_TEXT: 'To get started, Sign in with your @isb.pgp.edu account',
   CTA: {
